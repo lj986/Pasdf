d367edf815fff97a31baeae60c5af326a65b77b4
Jason Leung <abyssknight6@gmail.com>
2018-07-14 11:36:02 -0400

    CSS changes

--- assets/js/pokejs.js
+++ assets/js/pokejs.js
@@ -1,5 +1,6 @@
 function initializeBattle(p, o){
 	audio.play();
+	$("li").fadeIn();	
 	
 	$("#player .name").text(p.name);
 	$("#player .currentHP").text(p.hp)
@@ -12,8 +13,9 @@ function initializeBattle(p, o){
 	$("#opponent img").attr("src", o.img)
 	
 	toggleContent();
-
-	$("#msg").text("What will you do?");
+	
+	$("#msg").text("");
+	$("#msg").text("What should " + $("#player .name").text() + " do?")
 	defaultMenu();
 }
 
@@ -21,10 +23,11 @@ function strToVar(moveStr){ return eval(moveStr.replace(/\s+/g, '')); }
 
 function menu(command){
 	//executes command based on button text
-
+	click.play();
 	switch (command){
 		case "Fight":
 			messageBox("What should " + plrPoke.name + " do?")
+			$("#msg").clearQueue();
 			fightMenu(plrPoke, oppPoke); break;
 		case "Pokemon":
 			messageBox("You have no Pokemon!"); break;
@@ -33,7 +36,7 @@ function menu(command){
 		case "Run":
 			messageBox("You can't escape from this fight!"); break;
 		default:
-			//sets a variable move to the pokemon move object
+			//sets a variable move to the move object based on menu string
 			var move = strToVar(command);
 			round(plrPoke, oppPoke, move);	
 	}
@@ -68,141 +71,82 @@ function toggleMenu(){
 }
 
 function toggleContent(){
-	$("#pick").fadeToggle();
-	$("#battle").fadeToggle();
+	$("#pick").fadeToggle(0);
+	$("#battle").fadeToggle(0);
 }
 
 function messageBox(str){
 	$("#msg").queue(function(){
 		$("#container").one("click", function(){
+			click.play();
+			$("#msg").text(""); 
 			$("#msg").text(str); 
 			$("#msg").dequeue();
-			checkQueue();
-		});
+		})
 	})
-}
-
-function checkQueue(){
-	//checks if the queue is in progress and creates a blinking marker for the user if it is
-	var queueEmpty = $("#msg").queue()[0];
-	if(queueEmpty === "inprogress"){
-		$("#blinker").addClass("blinking")
-	} else {
-		$("#blinker").removeClass("blinking")
-	}
-	click.play();
 }
 
 function moveFirst(p, o){return (p.spd >= o.spd)};
 
 function round(p, o, m){
 	toggleMenu();
-	if (moveFirst(p,o)){
+	if ( moveFirst(p, o) ){
 		playerMove(p, o, m);
-		if (!gameOver){
+		if(!gameOver){
 			opponentMove(p, o);
 		}
 	} else {
 		opponentMove(p, o);
 		if(!gameOver){
-			playerMove(p, o, m);
+			playerMove(p, o, m); 
 		}
 	}
-	
+
+	//starts a new round by resetting the menu
 	if(!gameOver){
 		$("#msg").queue(function(){
 			$("#container").one("click", function(){
-				$("#msg").text("What will you do?");
+				$("#msg").text("What should " + $("#player .name").text() + " do?")
 				toggleMenu();
 				$("#msg").dequeue();
-				checkQueue();
-			});
+			})
 		})
 	}
 }
 
 function playerMove(p, o, m){
-	var multiplier = effectiveness(m, o);
-	var dmg = calcDamage(o, m, p);
-	
-	if (multiplier <= 0.5){
-		messageBox(p.name + " used " + m.name + "! It was not very effective...");
-	} else if (multiplier >= 2){
-		messageBox(p.name + " used " + m.name + "! It was super effective!");
-	} else {
-		messageBox(p.name + " used " + m.name + "!");
-	}
-
 	$("#opponent img").removeClass("ouch")
+	moveMessage(effectiveness(m, o), p, m)
 	
 	$("#msg").queue(function(){
-		setTimeout(function(){
-			$("#opponent .currentHP").text(Math.floor(Number($("#opponent .currentHP").text()) - dmg))
-			$("#opponent img").addClass("ouch")
-			$("#msg").dequeue();
-		},1500) 
-	});
-	
-	$("#msg").queue(function(){
-		setTimeout(function(){
-			if ($("#opponent .currentHP").text() < 1){
-				endGame("opponent");
-				audio.pause()
-				victory.play()
-			}
-			$("#msg").dequeue();
-		},0) 
-	});
+		dealDamage("opponent", calcDamage(o, m, p))
+		$("#msg").dequeue();
+	})
+		
 }
 
 function opponentMove(p, o){
-	moveNum = Math.floor(Math.random() * 3) 
-	m = eval(o.move[moveNum].replace(/\s+/g, ''))
-	var multiplier = effectiveness(m, p);
-	var dmg = calcDamage(p, m, o)
-	
-	if (multiplier <= 0.5){
-		messageBox(o.name + " used " + m.name + "! It was not very effective...");
-	} else if (multiplier >= 2){
-		messageBox(o.name + " used " + m.name + "! It was super effective!");
-	} else {
-		messageBox(o.name + " used " + m.name + "!");
-	}
-	
-	$("#player img").removeClass("ouch")
+	//randomly generate opponent's move
+	moveNum = Math.round(Math.random() * 3) 
+	var mov = eval(o.move[moveNum].replace(/\s+/g, ''));
 
-	$("#msg").queue(function(){
-		setTimeout(function(){
-			$("#player .currentHP").text(Math.floor(Number($("#player .currentHP").text()) - dmg))
-			$("#player img").addClass("ouch")
-			$("#msg").dequeue();
-		},500) 
-	});
+	$("#player img").removeClass("ouch")
+	moveMessage(effectiveness(mov, p), o, mov)
 	
 	$("#msg").queue(function(){
-		setTimeout(function(){
-			if($("#player .currentHP").text() < 1){
-				endGame("player");
-				audio.pause()
-			}
-			$("#msg").dequeue();
-		},0) 
-	});
+		dealDamage("player", calcDamage(p, mov, o))
+		$("#msg").dequeue();
+	})
 }
 
-function endGame(str){
-	$("#msg").clearQueue();
-	
-	if (str === "opponent"){
-		$("#opponent .currentHP").text(0);
-		messageBox("You won!!");
-	} else if (str === "player"){
-		$("#player .currentHP").text(0);
-		messageBox("You whited out!");
+function moveMessage(multiplier, user, move){
+	if (multiplier <= 0.5){
+		messageBox(user.name + " used " + move.name + "! It was not very effective...");
+	} else if (multiplier >= 2){
+		messageBox(user.name + " used " + move.name + "! It was super effective!");
+	} else {
+		messageBox(user.name + " used " + move.name + "!");
 	}
-	
-	$("ul").fadeToggle();
-	gameOver = true;
 }
 
 function calcDamage(target, move, source){
@@ -210,8 +154,7 @@ function calcDamage(target, move, source){
 	var total = 0;
 	var sourceAtk = 0;
 	var targetDef = 0;
-	var type = effectiveness(move, target);
-	var rand = (Math.random() * (100 - 85 + 1) + 85)/100
+	var rand = (Math.random() * 16 + 85)/100;
 
 	if (move.physical){
 		sourceAtk = source.atk;
@@ -221,8 +164,20 @@ function calcDamage(target, move, source){
 		targetDef = target.sdef;
 	}
 	
-	total = ((22 * move.power * (sourceAtk/targetDef))/50) * (rand * type);
+	total = ((22 * move.power * (sourceAtk/targetDef))/50) * (rand * effectiveness(move, target));
 	return total;
+}
+
+function dealDamage(target, dmg){
+	setTimeout(function(){
+		$("#"+target+" .currentHP").text(Math.round(Number($("#"+target+" .currentHP").text()) - dmg));
+		//checks if game is over by comparing the target's health
+		if($("#"+target+" .currentHP").text() < 0){
+			endGame(target);
+		}
+		$("#"+target+" img").addClass("ouch");
+		hit.play();
+	},100)
 }
 
 function effectiveness(move, target){
@@ -244,27 +199,67 @@ function effectiveness(move, target){
 	return multiplier;
 }
 
+function endGame(str){
+	audio.pause();
+	$("#msg").clearQueue();
+	$("#container").off()
+	
+	if (str === "opponent"){
+		$("#opponent .currentHP").text(0);
+		messageBox("You won!!");
+		victory.play();
+	} else if (str === "player"){
+		$("#player .currentHP").text(0);
+		messageBox("You whited out!");
+	}
+	
+	$("li").fadeOut();
+	gameOver = true;
+
+	messageBox("Click to play again");
+
+	
+	$("#msg").queue(function() {
+		$("#container").one("click", function(){
+			victory.pause();
+			victory.currentTime = 0;
+			audio.currentTime = 0;
+			gameOver = false;
+			toggleContent(); 
+		});
+	});
+}
+
 var plrPoke;
 var oppPoke;
 var gameOver = false;
 $("#battle").fadeToggle(0);
-	
-var audio = new Audio('assets/audio/battletheme.mp3');
-audio.volume = 0.1;
-var click = new Audio('assets/audio/click.mp3');
-click.volume = 0.4;
-var victory = new Audio('assets/audio/victory.mp3');
-victory.volume = 0.1;
+var n = 0;
+var pokeArray = [Blastoise, Charizard, Venusaur, Pikachu, Onix, Pidgeot];
+
+//Display list of pokemon to choose from
+for(var i = 0; i < pokeArray.length; i++){
+	$("form").append(
+		"<input type='radio' name='pokemon' id='"+pokeArray[i].name+"' value='"+pokeArray[i].name+"'/> <label for='"+pokeArray[i].name+"'> <img src='"+ pokeArray[i].img +"'/></label>"
+	);
+}
 	
 //sets player pokemon to pokemon user clicked and opponent's pokemon to a random one
 $('input:radio[name="pokemon"]').change( function(){
 	if ($(this).is(':checked')) {
-		plrPoke = strToVar(this.value)
-		var pokeArray = [Blastoise, Charizard, Venusaur, Pikachu, Onix, Pidgeot];
+		plrPoke = strToVar(this.value);
 		oppPoke = pokeArray[Math.floor(Math.random() * pokeArray.length)];
 		initializeBattle(plrPoke, oppPoke);
 		$("#battle button").on("click", function(){menu($(this).text())});
-		$(this).prop("checked", false)
-
+		$(this).prop("checked", false);
 	}
 });
+
+var audio = new Audio('assets/audio/battletheme.mp3');
+audio.volume = 0.1;
+var click = new Audio('assets/audio/click.mp3');
+click.volume = 0.3;
+var victory = new Audio('assets/audio/victory.mp3');
+victory.volume = 0.1;
+var hit = new Audio('assets/audio/hit.wav');
+hit.volume = 0.3;
